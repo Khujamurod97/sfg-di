2ecaecbdec82e4369005dfb85ed326e032aa302a
Khujamurod <mr.khujamurod.97@mail.ru>
2022-08-03 19:00:14 +0500

    init

--- src/main/java/uz/khujamurod/sfgdi/SfgDiApplication.java
+++ src/main/java/uz/khujamurod/sfgdi/SfgDiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SfgDiApplication.class, args);
+    }
 
 }
