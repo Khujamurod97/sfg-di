a5e36fb625ce8a51c5ae38116dbbff640214e0d1
Khujamurod <mr.khujamurod.97@mail.ru>
2022-08-04 15:18:32 +0500

    41 spring bean lifecycle

---
+++ src/main/java/uz/khujamurod/sfgdi/services/LifeCycleDemoBean.java
@@ -0,0 +1,47 @@
+package uz.khujamurod.sfgdi.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware,
+        BeanFactoryAware, ApplicationContextAware {
+    public LifeCycleDemoBean() {
+        System.out.println("##I'm in then LifeCycleDemoBean constructor");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("## Bean factory has been set");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("## My Bean name is " + s);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("## after property set");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("## Application Context has been set");
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("## The post constructor annotation method has been called");
+    }
+}
